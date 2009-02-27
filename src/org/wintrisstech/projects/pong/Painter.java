5bd8a01056b81726421007bd1e7134604a2a1182
Aaron VonderHaar <gruen0aermel@gmail.com>
2009-02-27 03:04:48 +0000

    Remove Graphics2D and casting.
    git-svn-id: http://school.wintrisstech.org/svn/repo/projects/Pong/Pong-Reference@1935 8d7e2954-883e-0410-b4ef-8fdef356481e

--- src/org/wintrisstech/projects/pong/Painter.java
+++ src/org/wintrisstech/projects/pong/Painter.java
@@ -1,11 +1,8 @@
 package org.wintrisstech.projects.pong;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import javax.swing.JComponent;
 
 public class Painter extends JComponent
@@ -15,47 +12,42 @@ public class Painter extends JComponent
     @Override
     protected void paintComponent(Graphics g)
     {
-        // This is how you create the g2 object.
-        Graphics2D g2 = (Graphics2D) g;
-
-        // Optionally show how you can clean up the picture with this.
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
         // Draw the scoreboard
-        g2.setColor(Color.green);
-        g2.setFont(new Font("Ariel", Font.BOLD, 22));
-        g2.drawString("Your Score Is ", 1700, 300);
-        g2.setColor(Color.black);
-        g2.setFont(new Font("Ariel", Font.ITALIC, 32));
-        g2.drawString("" + game.score, 1900, 300);
+        g.setColor(Color.green);
+        g.setFont(new Font("Ariel", Font.BOLD, 22));
+        g.drawString("Your Score Is ", 1700, 300);
+        g.setColor(Color.black);
+        g.setFont(new Font("Ariel", Font.ITALIC, 32));
+        g.drawString("" + game.score, 1900, 300);
 
         // Draw the ball speed indicator
-        g2.setColor(Color.red);
-        g2.drawString("Ball Speed Is     " + game.ball.xSpeed, 1700, 370);
+        g.setColor(Color.red);
+        g.drawString("Ball Speed Is     " + game.ball.xSpeed, 1700, 370);
 
-        // Draw the ball outline
-        g2.setColor(Color.black);
-        g2.setStroke(new BasicStroke(6f));
+        // Get ready to draw the ball
         Ball ball = game.ball;
-        g2.drawOval(ball.x, ball.y, ball.diameter, ball.diameter);
         // Draw the ball color
-        g2.setColor(Color.yellow);
-        g2.fillOval(ball.x, ball.y, ball.diameter, ball.diameter);
+        g.setColor(Color.yellow);
+        g.fillOval(ball.x, ball.y, ball.diameter, ball.diameter);
+        // Draw the ball outline
+        g.setColor(Color.black);
+        g.drawOval(ball.x, ball.y, ball.diameter, ball.diameter);
 
-        // Draw the paddle outline
+        // Get ready to draw the paddle
         Paddle p = game.paddle;
-        g2.setColor(Color.black);
-        g2.drawRect(p.x, p.y, p.width, p.height);
         // Draw the paddle color
-        g2.setColor(Color.gray);
-        g2.fillRect(p.x, p.y, p.width, p.height);
+        g.setColor(Color.gray);
+        g.fillRect(p.x, p.y, p.width, p.height);
+        // Draw the paddle outline
+        g.setColor(Color.black);
+        g.drawRect(p.x, p.y, p.width, p.height);
 
         // Draw the "YOU LOSE!" message, but only if the game is over
         if (game.gameOver)
         {
-            g2.setColor(Color.BLACK);
-            g2.setFont(new Font("Ariel", Font.BOLD, 300));
-            g2.drawString("YOU LOSE!", 50, 500);
+            g.setColor(Color.BLACK);
+            g.setFont(new Font("Ariel", Font.BOLD, 300));
+            g.drawString("YOU LOSE!", 50, 500);
         }
     }
 }
